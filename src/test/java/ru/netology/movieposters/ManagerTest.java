4ed3e6f622b6c4c6f4f515638646dff45764f030
IlyaNov <noil.01@mail.ru>
2023-04-15 16:23:04 +0300

    ManagerFixCI

--- src/test/java/ru/netology/movieposters/ManagerTest.java
+++ src/test/java/ru/netology/movieposters/ManagerTest.java
@@ -1,7 +1,6 @@
 package ru.netology.movieposters;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ManagerTest {
